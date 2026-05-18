c4f9b289a90bd79e398f4c87555e1016adda01bb
steve wagner <ciroque@live.com>
2026-05-17 18:51:06 -0700

    Initial commit

---
+++ lib/where_we_are/application.ex
@@ -0,0 +1,33 @@
+defmodule WhereWeAre.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      WhereWeAreWeb.Telemetry,
+      {DNSCluster, query: Application.get_env(:where_we_are, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: WhereWeAre.PubSub},
+      # Start a worker by calling: WhereWeAre.Worker.start_link(arg)
+      # {WhereWeAre.Worker, arg},
+      # Start to serve requests, typically the last entry
+      WhereWeAreWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: WhereWeAre.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    WhereWeAreWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ lib/where_we_are_web/endpoint.ex
@@ -0,0 +1,48 @@
+defmodule WhereWeAreWeb.Endpoint do
+  use Phoenix.Endpoint, otp_app: :where_we_are
+
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  @session_options [
+    store: :cookie,
+    key: "_where_we_are_key",
+    signing_salt: "sPqZGH+B",
+    same_site: "Lax"
+  ]
+
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [connect_info: [session: @session_options]],
+    longpoll: [connect_info: [session: @session_options]]
+
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phx.digest
+  # when deploying your static files in production.
+  plug Plug.Static,
+    at: "/",
+    from: :where_we_are,
+    gzip: false,
+    only: WhereWeAreWeb.static_paths()
+
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
+    plug Phoenix.CodeReloader
+  end
+
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Phoenix.json_library()
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
+  plug WhereWeAreWeb.Router
+end
