52844a3da430e739b513180c74f194b134c43497
steve wagner <ciroque@live.com>
2026-05-24 11:25:34 -0700

    Add calendar navigation

--- test/where_we_are_web/controllers/page_controller_test.exs
+++ test/where_we_are_web/controllers/page_controller_test.exs
@@ -10,10 +10,23 @@ defmodule WhereWeAreWeb.PageControllerTest do
       |> get(~p"/")
 
     month_label = Calendar.strftime(today, "%B %Y")
+    first_of_month = Date.beginning_of_month(today)
+    prev_month = first_of_month |> Date.add(-1) |> Date.beginning_of_month()
+
+    next_month =
+      first_of_month
+      |> Date.add(Date.days_in_month(first_of_month))
+      |> Date.beginning_of_month()
 
     response = html_response(conn, 200)
 
     assert response =~ month_label
+    assert response =~ "Move Previous"
+    assert response =~ "Move Next"
+    assert response =~ "Today"
+    assert response =~ ~s(href="/?month=#{Date.to_iso8601(prev_month)}")
+    assert response =~ ~s(href="/?month=#{Date.to_iso8601(next_month)}")
+    assert response =~ ~s(href="/?today=true")
 
     assert response =~ "Sun"
     assert response =~ "Mon"
@@ -22,5 +35,31 @@ defmodule WhereWeAreWeb.PageControllerTest do
     assert response =~ "Thu"
     assert response =~ "Fri"
     assert response =~ "Sat"
+  end
+
+  test "GET / with month param shows requested month", %{conn: conn} do
+    today = ~D[2024-01-15]
+    displayed_month = ~D[2024-03-01]
+
+    conn =
+      conn
+      |> Plug.Conn.assign(:today, today)
+      |> get(~p"/?month=#{Date.to_iso8601(displayed_month)}")
+
+    month_label = Calendar.strftime(displayed_month, "%B %Y")
+    prev_month = displayed_month |> Date.add(-1) |> Date.beginning_of_month()
+
+    next_month =
+      displayed_month
+      |> Date.add(Date.days_in_month(displayed_month))
+      |> Date.beginning_of_month()
+
+    response = html_response(conn, 200)
+
+    assert response =~ month_label
+    assert response =~ "Today"
+    assert response =~ ~s(href="/?month=#{Date.to_iso8601(prev_month)}")
+    assert response =~ ~s(href="/?month=#{Date.to_iso8601(next_month)}")
+    assert response =~ ~s(href="/?today=true")
   end
 end
