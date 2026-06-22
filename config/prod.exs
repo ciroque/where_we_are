d93bd4b765835d681e8eee8d61d01ac103d4d663
steve wagner <ciroque@live.com>
2026-06-22 14:43:36 -0700

    Checkpointing

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :where_we_are, WhereWeAreWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_endpoint: false
 
 # Do not print debug messages in production
 config :logger, level: :info
--- config/runtime.exs
+++ config/runtime.exs
@@ -38,6 +38,8 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
+  IO.puts("PHX_HOST:" <> host)
+
   config :where_we_are, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :where_we_are, WhereWeAreWeb.Endpoint,
