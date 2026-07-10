3824000782f25cf399d87f1ff73131d087b4ff5a
steve wagner <ciroque@live.com>
2026-07-09 19:32:45 -0700

    Remove invalid key

--- config/prod.exs
+++ config/prod.exs
@@ -6,8 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :where_we_are, WhereWeAreWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  check_endpoint: false
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
